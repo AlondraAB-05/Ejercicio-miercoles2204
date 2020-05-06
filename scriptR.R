957a1d778eec6e0f5f070e4f512f7380f84345c3
Alondra Aviña <aavina@utem.cl>
2020-05-06 17:09:17 -0500

    Primeros acercamientos con R

---
+++ scriptR.R
@@ -0,0 +1,23 @@
+a <- 6
+b <- 5
+c <- 4
+
+a + b
+
+a <= b
+c <= b && b <= a
+
+# se va a corroborar si un número es par 
+a %% 2== 0
+d %% 2 == 0
+388 %% 2 == 0
+
+a <- "hola a todos"
+a
+
+a <- TRUE
+a
+!a
+
+a <- 0.4
+a + b
