a8559be2afb76d7fd20652fbc9bb21600593109e
Alondra Aviña <aavina@utem.cl>
2020-05-06 18:35:06 -0500

    Se vieron las funciones if and else en R

---
+++ ClaseR,if,else.R
@@ -0,0 +1,108 @@
+if(2<4){
+  var1 <- 2 + 2
+}
+
+Var1 <- 2
+
+if(var1 == 2) {
+  var1 <- var1 + 2
+}
+var1
+
+
+
+if(var1 == 2) {
+  var1 <- var1 + 1
+
+
+if(var1 %% 2 == 0){
+  var1 <- var1 + 1
+}
+
+VarX <- 5
+VarX
+
+VarX <- 4
+
+if(VarX %% 2 == 0){
+  print("El numero es par")
+  VarX * 2
+} else{
+  print("El numero es impar")
+  VarX *3
+}
+
+VarX
+
+
+
+#####
+
+x <- 0
+
+if (x <= 0){
+  x <- 1
+  print("x")
+} else {
+  print("x es mayor a cero")
+  2 * x ^ 2
+}
+
+
+
+
+#####
+
+if (x <= 0){
+  print("x es mayor a cero")
+  x <-  2 * x ^ 2
+} else {
+  print("x es mayor a cero")
+  x <- 1
+  print (x)
+}
+x 
+
+
+
+####
+
+y <- 5
+
+if(y==3){
+  print("y vale 3")
+} else if (y < 3){
+  print("y es menor a 3")
+} else {
+  print("y es mayor a 3")
+}
+
+
+
+####
+
+x <- 2
+
+if(x < 0){
+  print("1")
+} else if (x > 0){
+  print("2x^2")
+} else { (x=0)
+  print("5")
+}
+
+
+
+#####
+
+x <- -8
+
+if(x < 0){
+  print("x es menor a cero")
+  x <- 1
+} else if (x > 0){
+  print("x es 2x^2")
+} else {
+  print("x es cero")
+  x <- 5
+}
